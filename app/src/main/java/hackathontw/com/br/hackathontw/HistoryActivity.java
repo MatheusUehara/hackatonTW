3bf2b22d26364626aeec2f8e09b90ad273826e00
LegDev01 <legilmo.developer01@gmail.com>
2017-04-08 18:08:38 -0300

    Custom feedback layout

--- app/src/main/java/hackathontw/com/br/hackathontw/HistoryActivity.java
+++ app/src/main/java/hackathontw/com/br/hackathontw/HistoryActivity.java
@@ -1,11 +1,14 @@
 package hackathontw.com.br.hackathontw;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 /**
  * Created by GilsonDeOliveira on 08/04/2017.
@@ -33,13 +36,47 @@ public class HistoryActivity extends Activity
             }
         });
 
-        final Button btnStart = (Button) findViewById(R.id.btnStart);
-        btnStart.setOnClickListener(new View.OnClickListener() {
+//        final Button btnStart = (Button) findViewById(R.id.btnStart);
+//        btnStart.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v)
+//            {
+//                Intent tela2 = new Intent(HistoryActivity.this, MainActivity.class);
+//                startActivity(tela2);
+//            }
+//        });
+
+
+        Button showFeedbackDialog = (Button) findViewById(R.id.btnStart);
+        showFeedbackDialog.setOnClickListener(new View.OnClickListener() {
+
             @Override
-            public void onClick(View v)
-            {
-                Intent tela2 = new Intent(HistoryActivity.this, MainActivity.class);
-                startActivity(tela2);
+            public void onClick(View arg0) {
+
+//                final Dialog dialog = new Dialog(HistoryActivity.this);
+//                dialog.setContentView(R.layout.feedback_dialog);
+//                dialog.setTitle("Title...");
+//
+//                ImageView image = (ImageView) dialog.findViewById(R.id.image);
+//                image.setImageResource(R.drawable.card1);
+//
+//                TextView content = (TextView) dialog.findViewById(R.id.content);
+//                content.setText("lorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsum.");
+//
+//                Button dialogButton = (Button) dialog.findViewById(R.id.dialogButtonOK);
+//                dialogButton.setOnClickListener(new View.OnClickListener() {
+//                    @Override
+//                    public void onClick(View v) {
+//                        dialog.dismiss();
+//                    }
+//                });
+//                //dialog.getWindow().setBackgroundDrawableResource(R.color.colorCorrectAnswer);
+//                //dialog.getWindow().setBackgroundDrawableResource(R.color.colorPrimary);
+//                dialog.setCancelable(false);
+//                dialog.show();
+
+                FeedbackDialog dialog = new FeedbackDialog(HistoryActivity.this, true);
+                dialog.show();
             }
         });
     }
