5d0fcb1d940ee7823e296921e0fa5cf0b38ce4e9
MatheusUehara <matheuspitanco@gmail.com>
2017-04-09 04:16:26 -0300

    Commit com atualização de feedback dos cards, Labels SIM e NÃO e inclusão das imagens que faltaram nos commits anteriores.

--- app/src/main/java/hackathontw/com/br/hackathontw/Card.java
+++ app/src/main/java/hackathontw/com/br/hackathontw/Card.java
@@ -7,12 +7,51 @@ package hackathontw.com.br.hackathontw;
 public class Card {
     private int mResDrawableCard;
     private String mText;
+    private String feedBackMessage;
+
+
     private boolean mAnswer;
 
-    public Card(int resDrawableCard, String text, boolean answer){
-        mResDrawableCard = resDrawableCard;
-        mText = text;
-        mAnswer =  answer;
+    public Card(){}
+
+    public Card(int resDrawableCard, String text, boolean answer, String feedBackMessage){
+        this.mResDrawableCard = resDrawableCard;
+        this.mText = text;
+        this.mAnswer =  answer;
+        this.feedBackMessage = feedBackMessage;
+
+    }
+
+    public int getmResDrawableCard() {
+        return mResDrawableCard;
+    }
+
+    public void setmResDrawableCard(int mResDrawableCard) {
+        this.mResDrawableCard = mResDrawableCard;
+    }
+
+    public String getmText() {
+        return mText;
+    }
+
+    public void setmText(String mText) {
+        this.mText = mText;
+    }
+
+    public String getFeedBackMessage() {
+        return feedBackMessage;
+    }
+
+    public void setFeedBackMessage(String feedBackMessage) {
+        this.feedBackMessage = feedBackMessage;
+    }
+
+    public boolean ismAnswer() {
+        return mAnswer;
+    }
+
+    public void setmAnswer(boolean mAnswer) {
+        this.mAnswer = mAnswer;
     }
 
     public int getResDrawableCard(){
